2995ef3ff5687b1fc0b12693f109c1c6518a5017
k.lamparski <k.lamparski@ocado.com>
2016-10-27 14:22:06 +0200

    Updated dependencies

--- src/main/java/io/heynow/sink/email/EmailSinkApplication.java
+++ src/main/java/io/heynow/sink/email/EmailSinkApplication.java
@@ -4,17 +4,19 @@ import io.heynow.sink.email.service.EmailServiceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 @EnableConfigurationProperties({
-		EmailServiceProperties.class
+        EmailServiceProperties.class
 })
 public class EmailSinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmailSinkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSinkApplication.class, args);
+    }
 
 }
